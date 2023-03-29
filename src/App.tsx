250f1c36bfa5fc6cd8a67e252e4bf5ced2b5414e
Andreas Schujkow <andreas.schujkow@hivemq.com>
2023-03-29 16:21:06 +0200

    feat: add text boxes

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,22 @@ function App() {
                         <HiveMQLogo className="logo" />
                     </a>
 
-                    <div className="connection-details">
+                    <div className="navigation">
                         <Navigation>
                             <ConnectionStatus />
                         </Navigation>
+                    </div>
+
+                    <hr className="divider" />
+
+                    <div className="content">
+                        <h1 className="content-title">Distributed MQTT Text Editor</h1>
+
+                        <p className="content-description">
+                            <b>Lorem Ipsum</b> is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the <i>industry's</i> standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and <a href="https://www.hivemq.com/">scrambled</a> it to make a type specimen book.
+                        </p>
                     </div>
 
                     <Editor />
