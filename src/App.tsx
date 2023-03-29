841b5da57836ec94101017cb4c27a80e5848f438
Andreas Schujkow <andreas.schujkow@hivemq.com>
2023-03-29 11:07:41 +0200

    chore: add HiveMQ logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+/// <reference types="vite-plugin-svgr/client" />
+
 import './App.css'
 import { Editor } from './components/Editor'
 import { MqttClient, MqttSubscriber } from './components/MqttClient'
 import { DocumentId } from './components/DocumentId'
 
+import { ReactComponent as HiveMQLogo } from './assets/01-hivemq.svg'
+
 function App() {
   return (
         <div className="App">
+            <HiveMQLogo className="logo" />
             <MqttClient>
                 <MqttSubscriber/>
                 <DocumentId/>
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import topLevelAwait from "vite-plugin-top-level-await";
-import wasm from "vite-plugin-wasm";
+import topLevelAwait from 'vite-plugin-top-level-await'
+import wasm from 'vite-plugin-wasm'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [topLevelAwait(), wasm(), react()],
+  plugins: [topLevelAwait(), wasm(), react(), svgr()],
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     alias: {
       mqtt: 'mqtt/dist/mqtt.min.js',
     },
   },
-
   worker: {
     format: 'es',
-    plugins: [topLevelAwait(), wasm()]
+    plugins: [topLevelAwait(), wasm()],
   },
   optimizeDeps: {
     // This is necessary because otherwise `vite dev` includes two separate
     // versions of the JS wrapper. This causes problems because the JS
     // wrapper has a module level variable to track JS side heap
     // allocations, initializing this twice causes horrible breakage
-    exclude: ["@automerge/automerge-wasm"]
-  }
+    exclude: ['@automerge/automerge-wasm'],
+  },
 })
