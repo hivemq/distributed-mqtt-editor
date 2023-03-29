59d49c2acfa970d828b91694dfecc47dd3e1cf29
Stefan Frehse <stefan.frehse@hivemq.com>
2023-03-29 10:51:51 +0200

    fix: cleanup files for deployment

--- src/components/MqttClient.tsx
+++ src/components/MqttClient.tsx
@@ -1,34 +1,32 @@
 import React from 'react'
-import {Connector, useMqttState, useSubscription} from 'mqtt-react-hooks';
-import {MqttDocument} from "./MqttDocument";
+import { Connector, useMqttState } from 'mqtt-react-hooks'
 
-export function MqttClient(props) {
-    const MQTT_OPTIONS: any = {
-        clientId: `distributed-editor/${Math.random()}`,
-        protocol: 'wss',
-        hostname: import.meta.env.VITE_MQTT_HOSTNAME,
-        protocolVersion: 4,
-        port: 8884,
-        path: '/mqtt',
-        clean: true,
-        resubscribe: false,
-        keepalive: 60,
-        reconnectPeriod: 0,
-        username: import.meta.env.VITE_MQTT_USERNAME,
-        password: import.meta.env.VITE_MQTT_PASSWORD,
-    }
+export function MqttClient(props: any) {
+  const MQTT_OPTIONS: any = {
+    clientId: `distributed-editor/${Math.random()}`,
+    protocol: 'wss',
+    hostname: import.meta.env.VITE_MQTT_HOSTNAME,
+    protocolVersion: 4,
+    port: 8884,
+    path: '/mqtt',
+    clean: true,
+    resubscribe: false,
+    keepalive: 60,
+    reconnectPeriod: 0,
+    username: import.meta.env.VITE_MQTT_USERNAME,
+    password: import.meta.env.VITE_MQTT_PASSWORD,
+  }
 
-    const brokerUrl = `${import.meta.env.VITE_MQTT_HOSTNAME}:8884/mqtt`
+  const brokerUrl = `${import.meta.env.VITE_MQTT_HOSTNAME}:8884/mqtt`
 
-    return (
+  return (
         <Connector brokerUrl={brokerUrl} options={MQTT_OPTIONS}>
             {props.children}
         </Connector>
-    );
+  )
 }
 
-
 export function MqttSubscriber() {
-    const {connectionStatus} = useMqttState();
-    return <h1>{`Status: ${connectionStatus}`}</h1>;
+  const { connectionStatus } = useMqttState()
+  return <h1>{`Status: ${connectionStatus}`}</h1>
 }
